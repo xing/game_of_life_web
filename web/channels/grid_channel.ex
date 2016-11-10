9bb742ae7253031bbec04146fab78a802342e2c0
Charles Bernasconi <charles.bernasconi@xing.com>
2016-11-10 20:21:47 +0100

    Cleanup log and unused code

--- web/channels/grid_channel.ex
+++ web/channels/grid_channel.ex
@@ -10,19 +10,16 @@ defmodule GameOfLifeWeb.GridChannel do
   end
 
   def handle_in("ticker:stop", _, socket) do
-   Logger.info "handle_in ticker:stop"
    GameOfLife.Ticker.stop_ticker
    {:reply, :ok, socket}
  end
 
  def handle_in("ticker:start", _, socket) do
-   Logger.info "handle_in ticker:start"
    GameOfLife.Ticker.start_ticker
    {:reply, :ok, socket}
  end
 
   def handle_in("ticker:interval_update", %{"newInterval" => new_interval}, socket) do
-    Logger.info "handle_in ticker:interval_update new_interval: #{inspect new_interval}"
     GameOfLife.Ticker.set_interval(new_interval)
     {:reply, :ok, socket}
   end
